8cdcbb565d7da91e0b8a258d3a013b8916d32af8
Marius Schulz <mariusschulz@users.noreply.github.com>
2012-07-19 18:02:37 +0200

    Initial commit

---
+++ SpecificationTextToTestConverter/SpecificationTextToTestConverter/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SpecificationTextToTestConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
